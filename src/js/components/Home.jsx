4cf7e9cc4b70b6c2458beae12b386b2b1b7688fe
Marjoriecus <101658277+Marjoriecus@users.noreply.github.com>
2025-06-18 23:33:51 +0000

    needs some css work

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+const TrafficLight = () => {
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [color, setColor] = useState ("red")
+
+	return (
+		<div id="light">
+			<button type="button" id="redLight" style={{background : "red", boxShadow: color == "red" && "0 0 10px 15px white"}} onClick ={() => setColor ("red")}></button>
+			<button type="button" id="yellowLight" style={{background : "yellow", boxShadow: color == "yellow" && "0 0 10px 15px white"}} onClick ={() => setColor ("yellow")}></button>
+			<button type="button" id="greenLight" style={{background : "green", boxShadow: color == "green" && "0 0 10px 15px white"}} onClick ={() => setColor ("green")}></button>
 		</div>
+		
 	);
 };
 
-export default Home;
+export default TrafficLight;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,6 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+import TrafficLight from './components/Home.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<TrafficLight/>);
